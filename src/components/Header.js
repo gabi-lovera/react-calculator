88d196ef26986b7f853bb0d84d9077e454607444
Lovera Gabriel <gabrielloveraa@gmail.com>
2021-09-22 21:02:28 -0300

    add link to repository

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ import { Image } from "../styles/StyledComponents";
 export default function Header() {
   return (
     <div className="header">
-      
+      <a href="https://github.com/Gabsl09/react-calculator">
+        <Image src="./images/github.svg" />
+      </a>
     </div>
   );
 }
