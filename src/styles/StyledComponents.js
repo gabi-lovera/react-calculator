1218b31465eb7c260a1e4784a2165687a3d55bf3
Lovera Gabriel <gabrielloveraa@gmail.com>
2021-09-28 18:02:43 -0300

    add styles with styled components

--- src/styles/StyledComponents.js
+++ src/styles/StyledComponents.js
@@ -5,30 +5,28 @@ export const Container = styled.div`
   justify-content: center;
   align-content: center;
   width: 400px;
-  margin: 40px auto;
+  margin: 30px auto;
+  padding-top: 5px;
   grid-template-columns: repeat(4, 100px);
   grid-template-rows: minmax(120px, auto) repeat(5, 100px);
   box-shadow: 2px 2px 10px #333;
   border-radius: 10px;
-`;
-
-export const Image = styled.img`
-  margin: 5px;
-  width: 40px;
+  background-color: #3e8e70;
 `;
 
 export const Screen = styled.div`
-    grid-column: 1 / -1;
-    background-color: #3d596e;
-    display: flex;
-    justify-content: space-around;
-    flex-direction: column;
-    padding: 10px;
-    word-wrap: break-word;
-    word-breakL break-all;
-    text-align: right;
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
+  grid-column: 1 / -1;
+  padding: 16px;
+  display: flex;
+  margin: 10px 10px 20px;
+  flex-direction: column;
+  align-items: flex-end;
+  background-color: #363636;
+  border-radius: 32px;
+  text-align: right;
+  justify-content: space-between;
+  word-break: break-all;
+  box-shadow: 0px 0px 0px 10px #00000033;
 `;
 
 export const Previous = styled.div`
@@ -42,20 +40,48 @@ export const Current = styled.div`
 
 export const Button = styled.button`
   cursor: pointer;
-  font-size: 2rem;
-  border: 1px outset white;
-  outline: none;
-  background-color: #edf6fe;
-  &:hover {
-    background-color: #f8fcff;
+  margin: 5px;
+  padding: 0;
+  border-radius: 32px;
+  font-size: 1.5em;
+  border: none;
+  background-color: #00916e;
+  box-shadow: 5px 5px 10px -3px #00000040, -5px -5px 15px 3px #00ffc11f;
+  &:active {
+    background-color: #006f54;
   }
 
-  /* Operators Buttons */
+  &:focus {
+    outline: none;
+  }
+
   ${({ gridSpan }) => gridSpan && `grid-column: span ${gridSpan};`}
-  ${({ operation }) => operation && `background-color: #fcbf49;`}
-    ${({ control }) => control && `background-color: skyblue;`}
+  ${({ operation }) =>
+    operation &&
+    `background-color: #fcbf49; &:active {
+    background-color: #daa133;
+  }`}
+    ${({ control }) =>
+    control &&
+    `background-color: #dc3546; &:active {
+      background-color: #c40c1f;
+    }`} 
     ${({ equals }) =>
-    equals && `border-bottom-right-radius: 10px; background-color: #f77f00;`}
+    equals &&
+    ` background-color: #fcbf49; &:active {
+      background-color: #daa133;
+    }`}
     ${({ period }) =>
-    period && `border-bottom-left-radius: 10px; background-color: skyblue;`}
+    period &&
+    ` background-color: skyblue; &:active {
+      background-color: #7cc1dd;
+    }`}
+`;
+
+export const Image = styled.img`
+  margin: 5px;
+  width: 40px;
+  &:hover {
+    transform: scale(1.1);
+  }
 `;
